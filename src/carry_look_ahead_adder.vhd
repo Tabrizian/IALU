9498292d97e5680e7e653f4e45cbf382b375da17
Iman Tabrizian <tabrizian@outlook.com>
2016-05-12 21:45:34 +0430

    Adds carry look ahead adder.

--- src/carry_look_ahead_adder.vhd
+++ src/carry_look_ahead_adder.vhd
@@ -2,12 +2,27 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity carray_look_ahead_adder is
+entity carry_look_ahead_adder is
     port(
-            a : in STD_LOGIC_VECTOR(7 downto 0);
-            b : in STD_LOGIC_VECTOR(7 downto 0);
+            a : in STD_LOGIC_VECTOR(3 downto 0);
+            b : in STD_LOGIC_VECTOR(3 downto 0);
             carry_in : in STD_LOGIC;
             carry_out : out STD_LOGIC;
-            res : out STD_LOGIC_VECTOR(7 downto 0)
+            res : out STD_LOGIC_VECTOR(3 downto 0)
         );
-end carray_look_ahead_adder;
+end carry_look_ahead_adder;
+
+architecture gate_level of carry_look_ahead_adder is
+    component pi_gi_generator_4 is
+    port(
+            a : in STD_LOGIC_VECTOR(3 downto 0);
+            b : in STD_LOGIC_VECTOR(3 downto 0);
+            p : out STD_LOGIC_VECTOR(3 downto 0);
+            g : out STD_LOGIC_VECTOR(3 downto 0)
+        );
+    end component;
+
+    signal p,g : STD_LOGIC_VECTOR(3 downto 0);
+begin
+    pi_gi : pi_gi_generator_4 port map(a, b, p, g);
+end gate_level;
--- src/pi_gi_generator_8.vhd
+++ src/pi_gi_generator_8.vhd
@@ -2,16 +2,16 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity pi_gi_generator_8 is
+entity pi_gi_generator_4 is
     port(
-            a : in STD_LOGIC_VECTOR(7 downto 0);
-            b : in STD_LOGIC_VECTOR(7 downto 0);
-            p : out STD_LOGIC_VECTOR(7 downto 0);
-            g : out STD_LOGIC_VECTOR(7 downto 0)
+            a : in STD_LOGIC_VECTOR(3 downto 0);
+            b : in STD_LOGIC_VECTOR(3 downto 0);
+            p : out STD_LOGIC_VECTOR(3 downto 0);
+            g : out STD_LOGIC_VECTOR(3 downto 0)
         );
-end pi_gi_generator_8;
+end pi_gi_generator_4;
 
-architecture gate_level of pi_gi_generator_8 is
+architecture gate_level of pi_gi_generator_4 is
     component pi_gi_generator is
         port(
                 a : in STD_LOGIC;
@@ -22,7 +22,7 @@ architecture gate_level of pi_gi_generator_8 is
     end component;
 begin
     GENEREATE_PI_GI:
-    for i in 0 to 7 generate
+    for i in 0 to 3 generate
         pi_gi : pi_gi_generator port map(a(i), b(i), p(i), g(i));
     end generate GENEREATE_PI_GI;
 end gate_level;
