39b693402bc13b514b2d9e93f4c811cbed421293
Iman Tabrizian <tabrizian@outlook.com>
2016-05-22 18:42:56 +0430

    Adds select subtractor.

---
+++ src/select_subtractor.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity select_subtractor is
+    port(
+            a : in STD_LOGIC_VECTOR(3 downto 0);
+            b : in STD_LOGIC_VECTOR(3 downto 0);
+            res : out STD_LOGIC_VECTOR(3 downto 0);
+            bout : out STD_LOGIC
+        );
+end select_subtractor;
+
+architecture gate_level of select_subtractor is
+    component carry_look_ahead is
+        port(
+                a : in STD_LOGIC_VECTOR(3 downto 0);
+                b : in STD_LOGIC_VECTOR(3 downto 0);
+                command : in STD_LOGIC;
+                carry_in : in STD_LOGIC;
+                carry_out : out STD_LOGIC;
+                res : out STD_LOGIC_VECTOR(3 downto 0)
+            );
+    end component;
+    signal subtract : STD_LOGIC_VECTOR(3 downto 0);
+    signal carry_out : STD_LOGIC;
+begin
+    cla : carry_look_ahead port map(a, b, '1', '0', carry_out, subtract);
+    res <= a when carry_out = '1' else
+           subtract when carry_out = '1';
+    bout <= carry_out;
+end gate_level;
